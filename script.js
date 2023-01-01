db47ff46a20435bb08b7b0263a2f0d1e57102c54
Saman-Safaei <xsamansafaeix@gmail.com>
2023-01-01 18:40:11 +0330

    initialize elements and create audio api

---
+++ script.js
@@ -0,0 +1,9 @@
+const audioApi = new Audio('/assets/music/callisto.mp3')
+
+const playpauseButton = document.getElementById('playpauseBtn')
+const playpauseIcon = playpauseButton.querySelector('img')
+const forwardButton = document.getElementById('forwardBtn')
+const backwardButton = document.getElementById('backwardBtn')
+const discImg = document.getElementById('disc')
+const playedBarElem = document.getElementById('playedBar')
+
