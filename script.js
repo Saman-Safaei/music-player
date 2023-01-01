276b9c0b938bb429fa69167cc4a6258e0345d122
Saman-Safaei <xsamansafaeix@gmail.com>
2023-01-01 18:44:25 +0330

    add event for music end

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const audioApi = new Audio('/assets/music/callisto.mp3')
 
+const pauseIconPath = '/assets/svg/pause.svg'
+const playIconPath = '/assets/svg/play.svg'
+
 const playpauseButton = document.getElementById('playpauseBtn')
 const playpauseIcon = playpauseButton.querySelector('img')
 const forwardButton = document.getElementById('forwardBtn')
@@ -10,17 +13,25 @@ const playedBarElem = document.getElementById('playedBar')
 function togglePlay() {
   if (!audioApi.paused) {
     audioApi.pause()
-    playpauseIcon.src = '/assets/svg/play.svg'
+    playpauseIcon.src = playIconPath
     discImg.style.animationPlayState = 'paused'
   } else {
     audioApi.play()
-    playpauseIcon.src = '/assets/svg/pause.svg'
+    playpauseIcon.src = pauseIconPath
     discImg.style.animationPlayState = 'running'
   }
 }
 function timeplayUpdated() {
-  playedBarElem.style.width = `${(audioApi.currentTime / audioApi.duration) * 100}%`
+  playedBarElem.style.width = `${
+    (audioApi.currentTime / audioApi.duration) * 100
+  }%`
+}
+function musicEnded() {
+  playedBarElem.style.width = '0px'
+  playpauseIcon.src = playIconPath
+  audioApi.currentTime = 0
 }
 
 audioApi.addEventListener('timeupdate', timeplayUpdated)
+audioApi.addEventListener('ended', musicEnded)
 playpauseButton.addEventListener('click', togglePlay)
